c3926d29b507fdbf5baedb01158dc67e06ce7ee0
Thomas Fung <tfung@ualberta.ca>
2016-01-10 13:51:14 -0700

    updated post item style

--- index.php
+++ index.php
@@ -1,18 +1,38 @@
 <?php get_header(); ?>
 
-
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<?php if ( have_posts() ) : ?>
 		<div class="row">
 		  	<div class="span7">
-		  		<?php the_content(); ?>
+		  		<?php 
+	  				echo "<div class=\"post-item\">";
+	  				the_post(); 
+	  				the_content();
+
+	  				echo "<span class=\"post-meta\">Posted on "; 
+	  				the_time('m/j/y g:i A');
+	  				echo "</span>";
+
+	  				echo "</div>";
+
+		  			while ( have_posts() ) {
+		  				echo "<hr class=\"post-divider\" /><div class=\"post-item\">";
+		  				the_post(); 
+		  				the_content();
+
+		  				echo "<span class=\"post-meta\">Posted on "; 
+	  					the_time('m/j/y g:i A');
+	  					echo "</span>";
+
+		  				echo "</div>";
+			  		}
+		  		?>
 		  	</div>
 			<div class="span3 offset1">
 				<?php get_sidebar(); ?>
 			</div>
 		</div>
 
-
-	<?php endwhile; else: ?>
+	<?php else: ?>
 		<p><?php _e('Sorry, this page does not exist.'); ?></p>
 	<?php endif; ?>
 
