fc316df6cc9cb018402ed57675ba261cfd55cbd1
Thomas Fung <thomas.fung1@gmail.com>
2016-02-07 00:36:35 -0700

    removed sidebar, added max width for image for mobile view

--- index.php
+++ index.php
@@ -2,22 +2,20 @@
 
 	<?php if ( have_posts() ) : ?>
 		<div class="row">
-		  	<div class="col-md-8">
+		  	<div class="col-md-offset-1 col-md-10">
 		  	<?php while (have_posts()) : the_post(); ?>
+		  		<div class="post">
+					<h3 class="post-header"><a class="post-link" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+					<br>
+					<p><?php the_content(); ?></p>
 
-				<h3 class="post-header"><a class="post-link" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
-				<br>
-				<p><?php the_content(); ?></p>
 
+					<span class="post-meta"><?php the_time('m/j/y g:i A'); ?></span>
 
-				<span class="post-meta"><?php the_time('m/j/y g:i A'); ?></span>
-
-				<hr class="post-divider" />
+					<hr class="post-divider" />
+				</div>
 			<?php endwhile; ?>
 		  	</div>
-			<div class="col-md-4">
-				<?php get_sidebar(); ?>
-			</div>
 		</div>
 
 	<?php else: ?>
