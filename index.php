07c56d444efa229e64bda220b8639b193d855b5c
Thomas Fung <thomas.fung1@gmail.com>
2016-02-21 02:10:03 -0700

    refactored index and content page

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php 
 
 // Event Address is a mandatory field, thus if not empty custom fields are active
-$customFieldsActive = get_field('event_address');
+//$customFieldsActive = get_field('event_address');
 
 get_header(); 
 
 if (have_posts()) : 
   while (have_posts()) :  
-    if ($customFieldsActive) :
-      get_template_part('templates/post templates/content', 'custom-fields');
-    else:
-      get_template_part('templates/post templates/content');
-    endif;
+    get_template_part('templates/post templates/content');
   endwhile; 
 else: 
   get_template_part('templates/error');
--- templates/post templates/content.php
+++ templates/post templates/content.php
@@ -1,17 +1,31 @@
-<?php the_post(); ?>
+<?php 
+$date_array = date_parse(get_field('event_date'));
+$date_str = ($date_array['error_count'] == 0 
+                ? date('l, F d, o', mktime(0, 0, 0, $date_array['month'], $date_array['day'], $date_array['year']))
+                : null);
+$google_location = get_field('google_location');
+$event_image = get_field('event_image');
+$event_time = get_field('event_time');
 
-<div class="row">
+the_post(); 
+?>
+
+<div class="row" style="margin: 40px 0;">
   <div class="col-md-offset-2 col-md-8">
-    <div class="post">
-      <h3 class="post-header"><a class="post-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-      <br>
-      <p><?php the_content(); ?></p>
-
-
-      <span class="post-meta"><?php the_time('m/j/y g:i A'); ?></span>
-
-      <hr class="post-divider" />
-    </div>
+    <h2 class="post-header"><a class="post-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php if ($date_str && $event_time): ?>
+        <h3 class="post-header"><?php echo $date_str; ?> | <?php echo $event_time; ?></h3>
+    <?php elseif ($date_str): ?>
+        <h3 class="post-header"><?php echo $date_str; ?></h3>
+    <?php elseif ($event_time): ?>
+        <h3 class="post-header"><?php echo $event_time; ?></h3>
+    <?php endif; ?>
+    
+    <?php if ($event_image) :?>
+        <img class="post-image" src="<?php echo $event_image['url']; ?>" alt="<?php the_title() ?>">
+    <?php endif; ?>
+    
+    <br/><br/>
+    <?php the_content(); ?>
   </div>
 </div>
-
