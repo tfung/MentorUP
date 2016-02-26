8d482a2d60b373be7174a1cd23688afaca5181cb
Thomas Fung <thomas.fung1@gmail.com>
2016-02-25 18:23:00 -0700

    fix regressed mobile sizing issue

--- page.php
+++ page.php
@@ -1,20 +1,22 @@
 <?php get_header(); ?>
 
-<div class="row">
-  <div class="col-md-offset-2 col-md-8">
+<div class="container">
+    <div class="row-fluid">
+      <div class="col-md-offset-2 col-md-8">
 
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <h1><?php the_title(); ?></h1>
-      <?php the_content(); ?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <h1><?php the_title(); ?></h1>
+          <?php the_content(); ?>
 
-    <?php endwhile; else: ?>
-    <p><?php _e('Sorry, this page does not exist.'); ?></p>
-  <?php endif; ?>
+        <?php endwhile; else: ?>
+        <p><?php _e('Sorry, this page does not exist.'); ?></p>
+      <?php endif; ?>
 
-</div>
-<!-- <div class="span4">
-    <h2>Sidebar</h2>    
-  </div> -->
+    </div>
+    <!-- <div class="span4">
+        <h2>Sidebar</h2>    
+      </div> -->
+    </div>
 </div>
 
 <?php get_footer(); ?>
--- templates/post templates/content.php
+++ templates/post templates/content.php
@@ -10,22 +10,28 @@ $event_time = get_field('event_time');
 the_post(); 
 ?>
 
-<div class="row" style="margin: 40px 0;">
-  <div class="col-md-offset-2 col-md-8">
-    <h2 class="post-header"><a class="post-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php if ($date_str && $event_time): ?>
-        <h3 class="post-header"><?php echo $date_str; ?> | <?php echo $event_time; ?></h3>
-    <?php elseif ($date_str): ?>
-        <h3 class="post-header"><?php echo $date_str; ?></h3>
-    <?php elseif ($event_time): ?>
-        <h3 class="post-header"><?php echo $event_time; ?></h3>
-    <?php endif; ?>
-    
-    <?php if ($event_image) :?>
-        <img class="post-image" src="<?php echo $event_image['url']; ?>" alt="<?php the_title() ?>">
-    <?php endif; ?>
-    
-    <br/><br/>
-    <?php the_content(); ?>
+<div class="post">
+  <div class="container">
+    <div class="row-fluid">
+      <div class="col-md-offset-2 col-md-8">
+        <h2 class="post-header"><a class="post-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <?php if ($date_str && $event_time): ?>
+            <h3 class="post-header"><?php echo $date_str; ?> | <?php echo $event_time; ?></h3>
+        <?php elseif ($date_str): ?>
+            <h3 class="post-header"><?php echo $date_str; ?></h3>
+        <?php elseif ($event_time): ?>
+            <h3 class="post-header"><?php echo $event_time; ?></h3>
+        <?php endif; ?>
+        
+        <?php if ($event_image) :?>
+            <img class="post-image" src="<?php echo $event_image['url']; ?>" alt="<?php the_title() ?>">
+        <?php endif; ?>
+        
+        <br/><br/>
+        <?php the_content(); ?>
+      </div>
+    </div>
   </div>
 </div>
+
+<hr class="post-divider"/>
