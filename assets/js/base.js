a03af849aff81cf7828fc32f23cdab5ce6d4e265
Thomas Fung <thomas.fung1@gmail.com>
2016-05-09 00:17:23 -0600

    Fixed mobile nabber view

--- assets/js/base.js
+++ assets/js/base.js
@@ -12,33 +12,44 @@
     }
   }
 
+  var setNavBarTheme = function () {
+    var isTop = !$(window).scrollTop();
+    var winWidth = window.innerWidth;
+
+    if (isTop && winWidth > 768) {
+      $('#navbar').removeClass('navbar-default');
+      $('#navbar').addClass('navbar-transparent');
+
+      $('#navbar .navbar-brand > img').removeClass('navbar-brand-fixedtop-img');
+      $('#navbar .navbar-brand > img').addClass('navbar-brand-img');
+
+      $('#navbar #navbar-list > #menu-main-menu').removeClass('navbar-fixedtop-margin');
+    } 
+    else {
+      $('#navbar').removeClass('navbar-transparent');
+      $('#navbar').addClass('navbar-default');
+
+      $('#navbar .navbar-brand > img').removeClass('navbar-brand-img');
+      $('#navbar .navbar-brand > img').addClass('navbar-brand-fixedtop-img');
+
+      $('#navbar #navbar-list > #menu-main-menu').addClass('navbar-fixedtop-margin');
+    }
+  }
+
   $(window).on('load', function () {
     adjustFooterPadding();
+
+    if (window.innerWidth <= 768) {
+      $('#navbar').removeClass('navbar-transparent');
+      $('#navbar').addClass('navbar-default');
+    }
   })
   .on('resize', function () {
     adjustFooterPadding();
+    setNavBarTheme();
   })
   .scroll(function() {
-      var isTop = !$(window).scrollTop();
-
-      if (isTop) {
-        $('#navbar').removeClass('navbar-default');
-        $('#navbar').addClass('navbar-transparent');
-
-        $('#navbar .navbar-brand > img').removeClass('navbar-brand-fixedtop-img');
-        $('#navbar .navbar-brand > img').addClass('navbar-brand-img');
-
-        $('#navbar #navbar-list > #menu-main-menu').removeClass('navbar-fixedtop-margin');
-      } 
-      else {
-        $('#navbar').removeClass('navbar-transparent');
-        $('#navbar').addClass('navbar-default');
-
-        $('#navbar .navbar-brand > img').removeClass('navbar-brand-img');
-        $('#navbar .navbar-brand > img').addClass('navbar-brand-fixedtop-img');
-
-        $('#navbar #navbar-list > #menu-main-menu').addClass('navbar-fixedtop-margin');
-      }
-    });
+    setNavBarTheme();
+  });
 
 })(jQuery);
